448406721fe1f94413b5e1fbae6f38e3414986ce
David Martín Matamala <64369027+DavidMM9@users.noreply.github.com>
2022-06-01 19:31:04 +0200

    Add files via upload

--- nba-stats-analyzer/src/agents/DataAgent.java
+++ nba-stats-analyzer/src/agents/DataAgent.java
@@ -1,7 +1,10 @@
 package agents;
 
 import behaviour.CyclicBehaviourReceiveMessage;
+import Jugadores.Jugador;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import jade.content.lang.sl.SLCodec;
 import jade.core.Agent;
 import jade.domain.DFService;
@@ -9,6 +12,10 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 /*
 Agente de percepcion de informacion. Recibe datos externos de las estadisticas de NBA que queremos procesar,
@@ -20,6 +27,9 @@ public class DataAgent extends Agent {
     private static final String SERVICE_NAME = "Data Communicator";
     private static final String SERVER_TYPE = "Raw Data Communication";
 
+
+    private ArrayList<Jugador> listaJugadores = new ArrayList<Jugador>();
+    private String filePath = "";
     /*
     TODO: investigar esta clase (no parece estar en la libreria
     private ChatJFrame charJFrame;
@@ -84,5 +94,36 @@ public class DataAgent extends Agent {
 
     private void loge(String s) {
         System.err.println(System.currentTimeMillis() + ": " + getLocalName() + "(" + getClass().getSimpleName() + ") " + s);
+    }
+
+    public ArrayList<Jugador> leerlista() throws FileNotFoundException, IOException{
+        ArrayList<Jugador> jugadores;
+        FileReader jugadoresFile = new FileReader(filePath);
+        Type tokenType = new TypeToken<ArrayList<Jugador>>(){}.getType();
+        jugadores = new Gson().fromJson(jugadoresFile, tokenType);
+        jugadoresFile.close();
+        return jugadores;
+    }
+
+    public void escribirLista(ArrayList<Jugador> jugadores) throws IOException{
+        FileWriter jugadoresFile = new FileWriter(filePath);
+        Gson gson = new Gson();
+        gson.toJson(jugadores,jugadoresFile);
+        jugadoresFile.close();
+    }
+
+    public void actualizarNota(String nombre, String apellido, float nota) throws IOException{
+
+        if(0 <= nota && nota <= 10){
+            for(int x = 0 ; x < listaJugadores.size(); x++){
+                Jugador j = listaJugadores.get(x);
+                if(j.getNombre().equals(nombre) && j.getApellido().equals(apellido)){
+                    j.setNota(nota);
+                }
+            }
+            escribirLista(listaJugadores);
+        }else{
+            System.err.println("Error: La nota debe estar comprendida entre 0 y 10");
+        }
     }
 }
